d6b46eb37329213458f5d4342ae09e38fdbd0dc0
Lakardi Mohamed Anis <127695733+lakardi-mohamed-anis@users.noreply.github.com>
2024-11-26 22:38:47 +0100

    l'ajout d'une version plus optimale

---
+++ VERSION OPTIMALE/src/gestion/d/etudiants/etudiant.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestion.d.etudiants;
+
+/**
+ *
+ * @author PC
+ */
+public class etudiant {
+    public String nom;
+    public String prenom;
+    public String dateN;
+    public String adresse;
+    
+    public etudiant(String nom, String prenom, String dateN, String adresse){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateN = dateN;
+        this.adresse = adresse;
+    }
+    
+}
